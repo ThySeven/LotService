3fcbeb9b92847fec52dd045547da5181cef6a2b9
Wufferman <eaajbst@students.eaaa.dk>
2024-05-15 12:36:15 +0200

    more logging

--- LotService/Services/LotService.cs
+++ LotService/Services/LotService.cs
@@ -72,11 +72,20 @@ namespace LotService.Services
         private async Task<UserModelDTO> FetchUserAsync(string bidderId)
         {
             var response = await WebManager.GetInstance.HttpClient.GetAsync($"http://{Environment.GetEnvironmentVariable("UserServiceEndpoint")}/user/{bidderId}");
-            AuctionCoreLogger.Logger.Info($"Lotservice call to {response.StatusCode}");
+            AuctionCoreLogger.Logger.Info($"Lotservice call to {response.RequestMessage.RequestUri} {response.StatusCode}");
             if (response.IsSuccessStatusCode)
             {
-                UserModelDTO user = await response.Content.ReadFromJsonAsync<UserModelDTO>();
-                AuctionCoreLogger.Logger.Info($"Fetched user from userservice: {JsonSerializer.Serialize(user)}");
+                UserModelDTO user = null;
+                try
+                {
+                    user = await response.Content.ReadFromJsonAsync<UserModelDTO>();
+                    AuctionCoreLogger.Logger.Info($"Fetched user from userservice: {JsonSerializer.Serialize(user)}");
+
+                }
+                catch(Exception ex)
+                {
+                    AuctionCoreLogger.Logger.Warn($"Failed to deserialize user form userservice: {ex}");
+                }
                 return user;
             }
             else
